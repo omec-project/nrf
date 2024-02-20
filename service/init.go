a488e01c183fed344def4c00b751985bfb8f25bb
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-02-20 16:59:55 -0500

    Fix linting error

--- service/init.go
+++ service/init.go
@@ -28,9 +28,9 @@ import (
 	"github.com/omec-project/nrf/logger"
 	"github.com/omec-project/nrf/management"
 	"github.com/omec-project/nrf/util"
+	mongoDBLibLogger "github.com/omec-project/util/logger"
 	"github.com/omec-project/util/path_util"
 	pathUtilLogger "github.com/omec-project/util/path_util/logger"
-	mongoDBLibLogger "github.com/omec-project/util/logger"
 )
 
 type NRF struct{}
