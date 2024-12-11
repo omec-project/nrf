a527d157e74cd2dc1222e91c72885d88f1f95574
Gulsum Atici <gulsum.atici@canonical.com>
2024-12-11 03:50:39 +0300

    fix: potential problems that may cause panic (#160)
    * fix potential problems that may cause panic
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * Start logging with lowercase letters
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * chore: remove empty spaces
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * chore: Add blank lines between logical blocks
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * Handle all unhandled errors in nf_management.go
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * chore: create version 1.6.1
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * fix: start logs with lowercase letters
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * fix: remove empty spaces
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    * fix: remove new line character
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    ---------
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- management/api_nf_instance_id_document.go
+++ management/api_nf_instance_id_document.go
@@ -24,7 +24,7 @@ import (
 	"github.com/omec-project/util/httpwrapper"
 )
 
-// DeregisterNFInstance - Deregisters a given NF Instance
+// HTTPDeregisterNFInstance - Deregisters a given NF Instance
 func HTTPDeregisterNFInstance(c *gin.Context) {
 	// parse nfInstanceId
 
@@ -47,7 +47,7 @@ func HTTPDeregisterNFInstance(c *gin.Context) {
 	}
 }
 
-// GetNFInstance - Read the profile of a given NF Instance
+// HTTPGetNFInstance - Read the profile of a given NF Instance
 func HTTPGetNFInstance(c *gin.Context) {
 	req := httpwrapper.NewRequest(c.Request, nil)
 	req.Params["nfInstanceID"] = c.Params.ByName("nfInstanceID")
@@ -68,7 +68,7 @@ func HTTPGetNFInstance(c *gin.Context) {
 	}
 }
 
-// RegisterNFInstance - Register a new NF Instance
+// HTTPRegisterNFInstance - Register a new NF Instance
 func HTTPRegisterNFInstance(c *gin.Context) {
 	var nfprofile models.NfProfile
 
@@ -124,7 +124,7 @@ func HTTPRegisterNFInstance(c *gin.Context) {
 	}
 }
 
-// UpdateNFInstance - Update NF Instance profile
+// HTTPUpdateNFInstance Update NF Instance profile
 func HTTPUpdateNFInstance(c *gin.Context) {
 	// step 1: retrieve http request body
 	requestBody, err := c.GetRawData()
@@ -151,7 +151,7 @@ func HTTPUpdateNFInstance(c *gin.Context) {
 		logger.ManagementLog.Warnln(err)
 		problemDetails := models.ProblemDetails{
 			Status: http.StatusInternalServerError,
-			Cause:  "SYSTEM_FAILURE",
+			Cause:  "SERIALIZATION_FAILURE",
 			Detail: err.Error(),
 		}
 		c.JSON(http.StatusInternalServerError, problemDetails)
