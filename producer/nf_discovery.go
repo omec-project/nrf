b30866a620da6d066a9b73f4db50c63b7a72ddc8
ReshmaJS <you@example.com>
2026-07-15 10:03:56 +0530

    lint issue fixed
    Signed-off-by: ReshmaJS <you@example.com>

--- producer/nf_discovery.go
+++ producer/nf_discovery.go
@@ -1708,6 +1708,7 @@ func complexQueryFilterSubprocess(queryParameters map[string]*AtomElem, complexQ
 
 	return filter
 }
+
 func addTargetNfTypeFilter(queryParameters map[string]*AtomElem, filter bson.M, logicalOperator string) string {
 	// [Query-1] target-nf-type
 	var targetNfType string
